4649003309cb79823d4d39efbc65b713ef4c0dd2
johnnyshields <27655+johnnyshields@users.noreply.github.com>
2025-08-07 23:09:27 +0900

    Improve code

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,17 +3,22 @@
 require "bundler"
 require "pry-byebug"
 
-require "active_support/all"
-activesupport_version = ActiveSupport.version.to_s
-
-# Clone the repo version of Rails to use its test helpers
-TMP_CLONE_DIR = File.join(Dir.tmpdir, "rails_#{activesupport_version}")
-unless Dir.exist?(TMP_CLONE_DIR)
-  puts "Cloning Rails repo to #{TMP_CLONE_DIR}..."
-  system("git clone --depth=1 --branch v#{activesupport_version} https://github.com/rails/rails.git #{TMP_CLONE_DIR}")
+def use_rails_repo(version = nil)
+  dir = File.join(Dir.tmpdir, "rails_#{version || 'main'}")
+  unless Dir.exist?(dir)
+    puts "Cloning Rails repo to #{dir}..."
+    system("git clone --depth=1#{" --branch v#{version}" if version} https://github.com/rails/rails.git #{dir}")
+  end
+  require "#{dir}/activesupport/test/testing/method_call_assertions_test"
+  require "#{dir}/activesupport/test/cache/behaviors"
 end
-require "#{TMP_CLONE_DIR}/activesupport/test/testing/method_call_assertions_test"
-require "#{TMP_CLONE_DIR}/activesupport/test/cache/behaviors"
+
+require "active_support/all"
+begin
+  use_rails_repo(ActiveSupport.version.to_s)
+rescue LoadError
+  use_rails_repo
+end
 
 # cache test behavior overrides
 require_relative "behaviors/composite_cache_store_behavior"
